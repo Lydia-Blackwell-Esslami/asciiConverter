8a5744e5b3097146d52e2a06e0ff7749d65a69af
Lydia_Blackwell-Esslami <lblackwellesslami@smith.edu>
2025-11-20 15:47:05 -0500

    loading function done

---
+++ R/load_files.R
@@ -0,0 +1,22 @@
+#' Load txt files into memory
+#'
+#' @param filenames String vector with the names of files to be added
+#'
+#' @returns A list with the same length as the input vector, where each element of the list
+#' is a vector and each element ofa vector is a line of a file
+#' @export
+#' 
+#' @details
+#' Use this to load in the entire library, and then work with it from the output list 
+#' 
+#' Example usage:
+#' your_files <- load_files(c("SomeFile.txt", "OtherFile.txt")))
+#' 
+load_files <- function(filenames){
+  letters_1 <-list()
+  for(x in filenames){
+    letters_2<- c(readLines(x, warn = FALSE))
+    letters_1 <- append(letters_1, list(letters_2))
+  }
+  return(letters_1)
+}
