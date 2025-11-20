f3a0a8c98084ab80b2e11446230e182d140f143d
smmaher <smmaher@smith.edu>
2025-11-20 15:04:59 -0500

    new

--- R/string_to_int.R
+++ R/string_to_int.R
@@ -11,12 +11,7 @@
 #' string_convert("ABCD")
 string_convert <- function(x){
   x <- strsplit(x, "")
-<<<<<<< HEAD
-  y <- purrr::map(x, utf8ToInt)
-  return(y - 32)
-=======
   y <- purrr::map(x[[1]], utf8ToInt)
   z <- unlist(y)
   return(z-32)
->>>>>>> 488791bdc8f25284edec3938dcfb234b57c82492
 }
